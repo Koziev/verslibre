7a6dc512ad074c57c8bc4b3ce84e47ea0b0f00f0
koziev ilya <kelijah@yandex.ru>
2022-05-08 09:03:16 +0300

    в первую порцию саджестов подмешиваются содержащие стем названия текущего месяца

--- py/generative_poetry/poetry_seeds.py
+++ py/generative_poetry/poetry_seeds.py
@@ -6,6 +6,8 @@
 04.04.2022 Первая (для данного пользователя) порция седжестов генерируется особым образом, а не берется
            из подготовленного списка.
 03.05.2022 Отдельный список затравок для бусидо
+08.05.2022 В первую порцию саджестов для пользователя подбираются саджесты из общего списка, содержащие
+           стем названия текущего месяца.
 """
 
 import random
@@ -41,18 +43,39 @@ class SeedGenerator(object):
             if len(seeds) >= 2:
                 return list(seeds)
 
-        if len(self.user_seeds) == 0:
+        cur_month = datetime.datetime.now().month
+
+        if len(self.user_seeds[user_id]) == 0:
             # 04.04.2022 отдельная ветка генерации первой порции саджестов.
-            cur_month = datetime.datetime.now().month
             adj_m, adj_f, adj_n, adj_p, noun_m, noun_f, noun_n, noun_p = self.month_2_data[cur_month]
+
+            selected_seeds = set()
 
             for _ in range(3):
                 adjs, nouns = random.choice([(adj_m, noun_m), (adj_f, noun_f), (adj_n, noun_n), (adj_p, noun_p)])
                 adj = random.choice(adjs)
                 noun = random.choice(nouns)
                 colloc = adj + ' ' + noun
-                seeds.add(colloc)
+                selected_seeds.add(colloc)
                 self.user_seeds[user_id].add(colloc)
+
+            # 08.05.2022 подбираем из всех саджестов такие, которые содержат некоторые ключевые слова типа название
+            # текущего месяца (точнее стем названия).
+            stem = ['январ', 'феврал', 'мартов', 'апрел', 'майск', 'июнь', 'июль', 'август', 'сентябр',
+                    'октябр', 'ноябр', 'декабр'][cur_month - 1]
+            for seed in self.common_seeds:
+                if stem in seed.lower():
+                    selected_seeds.add(seed)
+
+            for seed in sorted(selected_seeds, key=lambda _: random.random()):
+                if seed not in self.user_seeds[user_id] and seed.count(' ') <=2:
+                    seeds.add(seed)
+                    self.user_seeds[user_id].add(seed)
+                    if len(seeds) >= 3:
+                        break
+
+                if len(seeds) >= 3:
+                    return seeds
         else:
             # Вторая и последующие порции саджестов.
             if len(seeds) < 3:
